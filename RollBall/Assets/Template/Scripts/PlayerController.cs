96f1dd3c0797bb184edace86a9736975887a5c2d
lohare21 <lohare@iu.edu>
2022-02-19 19:48:36 -0500

    Code is right, but it still comes up with errors

--- RollBall/Assets/Template/Scripts/PlayerController.cs
+++ RollBall/Assets/Template/Scripts/PlayerController.cs
@@ -1,33 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+errusing System.Collections;  
+using System.Collections.Generic;  
 using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class NewBehaviourScript : MonoBehaviour
+using UnityEngine.InputSystem;  
+   
+public class PlayerController : MonoBehaviour 
 {
-
-    private Rigidbody rb;
+    private Rigidbody
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
+    
+    void start()
     {
-        rb = GetComponent<Rigidbody>();   
-    }
+        rb = GetComponent<Rigidbody>();
+    }  
+    
+
     void OnMove(InputValue movementValue)
     {
-       Vector2 movementvector = movementValue.Get<Vector2>(); 
+        Vector2 movementVector = movementValue.Get<Vector2>();
 
-       movementX = movementVector.x;
-       movementY = movementVector.y;
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 
-
-
-    void FixUpdate()
+    void FixedUpdate()
     {
-        Vector3 movment = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(movementx, 0.0f, movementY);
         
-        rb.AddForce(movement);
+        rb.AddForce(movementVector);
     }
 }
