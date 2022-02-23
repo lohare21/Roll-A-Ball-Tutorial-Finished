1846572cd1cbf80d7f69c4a5b11c4bd768ab5bb8
lohare21 <lohare@iu.edu>
2022-02-23 15:56:30 -0500

    got the code working, the only problem now is making cube disappear

--- RollBall/Assets/Scripts/PlayerController.cs
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -33,4 +33,11 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUP")) 
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
