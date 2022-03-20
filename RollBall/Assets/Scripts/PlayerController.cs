a043ee64fd7966f3f66ae933a8807118eb0d8537
lohare21 <lohare@iu.edu>
2022-03-20 03:39:36 -0400

    Ball can move

--- RollBall/Assets/Scripts/PlayerController.cs
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -2,27 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-    public TextMeshProUGUI countText;
-	public GameObject winTextObject;
 
     private Rigidbody rb;
-    private int count;
 
     private float movementX;
     private float movementY;
 
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
-
-        SetCountText ();
-        winTextObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementValue)
@@ -31,16 +24,6 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
-        
-    }
-
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if(count >= 12)
-        {
-            winTextObject.SetActive(true);
-        }   
     }
 
     private void FixedUpdate()
@@ -50,13 +33,4 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp")) 
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText ();
-        }
-    }
 }
